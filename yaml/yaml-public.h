a9877e57cd80f1edd486f8853bdfd358b6abb440
Trieu Nguyen <trieu.nguyen@geocomply.com>
2021-10-01 16:27:52 +0700

    Separate yaml to static library

---
+++ yaml/yaml-public.h
@@ -0,0 +1,14 @@
+//
+//  yaml-public.h
+//  yaml
+//
+//  Created by Trieu Nguyen on 01/10/2021.
+//
+
+#ifndef yaml-public_h
+#define yaml-public_h
+
+#import "YAMLSerialization.h"
+
+
+#endif /* yaml-public_h */
