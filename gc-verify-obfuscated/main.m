4eb8778f45a09d5100c35f8fc89955be0014733a
Trieu Nguyen <trieu.nguyen@geocomply.com>
2021-10-01 16:30:56 +0700

    Add gc-verify-obfuscated target

---
+++ gc-verify-obfuscated/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gc-verify-obfuscated
+//
+//  Created by Trieu Nguyen on 01/10/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
