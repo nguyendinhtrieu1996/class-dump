844d5356b3250fad3325a53da7caeba55115d984
Trieu Nguyen <trieu.nguyen@geocomply.com>
2021-10-05 10:01:10 +0700

    [gc-verify-obfuscated] parse arguments

--- gc-verify-obfuscated/main.m
+++ gc-verify-obfuscated/main.m
@@ -11,13 +11,38 @@
 #import "GCObfuscatedVerificationResponse.h"
 
 
+void print_usage(void)
+{
+    fprintf(stderr,
+            "\n\n---------------------------------------\n"
+            "gc-verify-obfuscated\n"
+            "Usage: gc-verify-obfuscated [blacklist file path] [dump filepath]"
+            );
+}
+
+#define GC_MIN_ARGC 3
+
+
 int main(int argc, const char * argv[]) {
     
     @autoreleasepool {
+        if (argc < GC_MIN_ARGC) {
+            print_usage();
+            exit(1);
+        }
+        
+        const char *blacklistFilePath = argv[argc - 2];
+        const char *dumpFilePath = argv[argc - 1];
+        if (blacklistFilePath == NULL || dumpFilePath == NULL) {
+            fprintf(stderr, "\n Invalid file path");
+            print_usage();
+            exit(1);
+        }
+        
         GCObfuscatedVerification *obfuscatedVerifycation = [GCObfuscatedVerification new];
         GCObfuscatedVerificationResponse *response
-        = [obfuscatedVerifycation verifyWithBlackListFile:@"/Users/geotech/Downloads/blacklist.yml"
-                                               dumpedPath:@"/Users/geotech/Downloads/dump"];
+        = [obfuscatedVerifycation verifyWithBlackListFile:[NSString stringWithUTF8String:blacklistFilePath]
+                                               dumpedPath:[NSString stringWithUTF8String:dumpFilePath]];
         
         printf("\n\n=======================VERIFY FINISH=======================\n\n");
         
